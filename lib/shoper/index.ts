599d83062662ab7420346e2c54109b2a2beaa4d5
Z4NR34L <software@zanreal.net>
2024-04-09 08:20:23 +0200

    preparing shoper integration

--- lib/shoper/index.ts
+++ lib/shoper/index.ts
@@ -1,3 +1,21 @@
-export default async function storeRequest() {
-  return fetch('https://localhost:8080');
+import { DEFAULT_TAGS } from './constants';
+
+interface StoreReqeustProps {
+  tags?: string[];
+  cache?: RequestCache;
+  revalidate?: number | false;
+}
+
+export default async function storeRequest({
+  tags,
+  cache,
+  revalidate
+}: Readonly<StoreReqeustProps>) {
+  return fetch(`https://${process.env.SHOPER_STORE_DOMAIN}/webapi/rest/`, {
+    cache,
+    next: {
+      tags: (tags ?? []).concat(DEFAULT_TAGS),
+      revalidate
+    }
+  });
 }
