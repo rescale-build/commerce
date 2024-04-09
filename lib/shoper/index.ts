a2dbe9ce74a8d279295b19735c5e3db9d2753069
Z4NR34L <software@zanreal.net>
2024-04-09 09:27:24 +0200

    preparing shoper SDK

--- lib/shoper/index.ts
+++ lib/shoper/index.ts
@@ -1,39 +1,105 @@
 import { DEFAULT_TAGS } from './constants';
+import { Product } from './types';
 
 interface StoreReqeustProps {
+  urlProps?: string[];
   tags?: string[];
   cache?: RequestCache;
   headers?: HeadersInit;
   revalidate?: number | false;
 }
 
-export default async function storeFetch({
-  tags,
-  cache,
-  headers,
-  revalidate
-}: Readonly<StoreReqeustProps>): Promise<Response> {
-  return fetch(`https://${process.env.SHOPER_STORE_DOMAIN}/webapi/rest/`, {
-    cache,
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.SHOPER_STORE_API_KEY}`,
-      ...headers
-    },
-    next: {
-      tags: (tags ?? []).concat(DEFAULT_TAGS),
-      revalidate
-    }
-  });
+export default async function storeFetch<T>(
+  endpoint: string,
+  { urlProps, tags, cache, headers, revalidate }: Readonly<StoreReqeustProps>
+): Promise<T | never> {
+  let url = `https://${process.env.SHOPER_STORE_DOMAIN}/webapi/rest/${endpoint}`;
+
+  if (urlProps) {
+    url = `${url}/${urlProps.join('/')}/`;
+  }
+
+  try {
+    const response = await fetch(url, {
+      cache,
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers
+      },
+      next: {
+        tags: (tags ?? []).concat(DEFAULT_TAGS),
+        revalidate
+      }
+    });
+
+    return response.json() as T;
+  } catch (e) {
+    throw {
+      error: e
+    };
+  }
 }
 
 export class Store {
-  constructor() {}
+  /**
+   * The store token used to authenticate requests to the Shoper API.
+   * @private
+   */
+  private storeToken?: string;
 
+  /**
+   * Fetches the store token from the Shoper API.
+   */
+  constructor() {
+    void this.getToken();
+  }
+
+  /**
+   * Fetches the store token from the Shoper API.
+   * @private
+   */
+  private async getToken() {
+    if (this.storeToken) {
+      return this.storeToken;
+    }
+
+    const response = await fetch(`https://${process.env.SHOPER_STORE_DOMAIN}/webapi/rest/`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.SHOPER_STORE_API_KEY}`
+      }
+    });
+
+    const { token } = await response.json();
+
+    this.storeToken = token;
+
+    return token;
+  }
+
+  /**
+   * The product endpoint.
+   */
   public product = {
-    get() {
-      return storeFetch({
-        tags: ['product']
+    endpoint: '/product',
+    get: async (): Promise<Product> => {
+      const token = await this.getToken();
+
+      return storeFetch<Product>(this.product.endpoint, {
+        tags: ['product'],
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    },
+    list: async (): Promise<Product[]> => {
+      const token = await this.getToken();
+
+      return storeFetch<Product[]>(this.product.endpoint, {
+        tags: ['product', 'product-list'],
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
       });
     }
   };
